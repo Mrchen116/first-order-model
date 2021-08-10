479e2421394703bfafb9719e9da6cc9f7cf29652
Mrchen116 <45305047+Mrchen116@users.noreply.github.com>
2021-08-10 16:56:34 +0800

    Update work.py

--- work.py
+++ work.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+import imutils
 import tensorflow as tf
 from tf_bodypix.api import download_model, load_model, BodyPixModelPaths
 
@@ -11,16 +12,21 @@ def separate(img, mask):
     return fg.astype(np.uint8), bg.astype(np.uint8)
 
 IMG_PATH = 'data/boy.png'
+IMG_RESIZE_PATH = 'log/resize.png'
 VIDEO_PATH = 'data/trump.mp4'
 FG_PATH = 'log/fg.png'
 BG_PATH = 'log/bg.png'
 CROP_RES_PATH = 'log/crop_result.mp4'
 
+origin = cv2.imread(IMG_PATH)
+resized = imutils.resize(origin, width=256)
+cv2.imwrite(IMG_RESIZE_PATH, resized)
+
 bodypix_model = load_model(download_model(
     BodyPixModelPaths.MOBILENET_FLOAT_100_STRIDE_8
 ))
 
-image = tf.keras.preprocessing.image.load_img(IMG_PATH)
+image = tf.keras.preprocessing.image.load_img(IMG_RESIZE_PATH)
 image_array = tf.keras.preprocessing.image.img_to_array(image)
 result = bodypix_model.predict_single(image_array)
 mask = result.get_mask(threshold=0.75)

0b7b3fa215aadfdd0e86dac961db55fb6110a41b
Mrchen116 <45305047+Mrchen116@users.noreply.github.com>
2021-08-12 14:35:28 +0800

    Update start.py
    debug

--- start.py
+++ start.py
@@ -15,20 +15,21 @@ parser.add_argument("--driving_video", default='data/trump.mp4', help="path to d
 parser.add_argument("--result_video", default='0', help="path to output")
 parser.add_argument("--cutted", action="store_true", help="if the iamge has been cutted")
 
+args = parser.parse_args()
 
-if not parser.cutted:
+if not args.cutted:
     with mp_face_detection.FaceDetection(
         model_selection=1) as face_detection:
-        image = cv2.imread(parser.source_image)
+        image = cv2.imread(args.source_image)
         result = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         assert result.detections
         for detection in result.detections:
             print(detection.xmin)
 
 exit(0)
-driving_video = parser.driving_video
-source_image = parser.source_image
-result = os.path.join('log', f"{getfilename(driving_video)}_{getfilename(source_image)}.mp4") if parser.result_video == '0' else parser.result_video
+driving_video = args.driving_video
+source_image = args.source_image
+result = os.path.join('log', f"{getfilename(driving_video)}_{getfilename(source_image)}.mp4") if args.result_video == '0' else args.result_video
 cmd = f"python demo.py  --config config/vox-256.yaml --driving_video {driving_video} " \
       f"--source_image {source_image} --checkpoint checkpoints/vox256.pth --relative --adapt_scale"
 print(cmd)
